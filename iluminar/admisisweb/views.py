0d18646d9cdbff5584ce970ef6393b9cc9221199
rodriguezmelisa15 <109873727+rodriguezmelisa15@users.noreply.github.com>
2023-05-29 16:53:25 -0300

    vista empresa y botones/conexion BD/

--- iluminar/admisisweb/views.py
+++ iluminar/admisisweb/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def inicio (request):
-    return HttpResponse ("<h1>Inicio</h1>)")
 
 def Empresa(request):
     return render (request, "paginas/empresa.html")
--- iluminar/iluminar/settings.py
+++ iluminar/iluminar/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'iluminar.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'iluminarpba',
+        'USER': 'melisaar',
+        'PASSWORD': 'Melusa2012!',
+        'HOST': '190.228.29.62',
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
